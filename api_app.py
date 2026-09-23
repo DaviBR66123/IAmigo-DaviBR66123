9aed7e0ae71eec3a36b27e45fdc84a17d2705853
Davi Branches <davi56961306@alunos.sc.senac.br>
2026-09-22 21:49:39 -0300

    Atualização do api_app.py

--- api_app.py
+++ api_app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from api.rotas_usuarios import router as usuarios_router
 from api.rotas_tarefas import router as tarefas_router
+from api.rotas_passos import router as passos_router
 
 app = FastAPI(
     title="TPaC API",
@@ -19,6 +20,7 @@ def inicio():
 
 app.include_router(usuarios_router)
 app.include_router(tarefas_router)
+app.include_router(passos_router)
 
 @app.get("/")
 def inicio():
